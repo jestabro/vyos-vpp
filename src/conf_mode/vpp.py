7f002ad07daa7a47eb90e3bf0a59019a110518d3
John Estabrook <jestabro@vyos.io>
2025-05-14 09:43:46 -0500

    raise error on reinit

--- src/conf_mode/vpp.py
+++ src/conf_mode/vpp.py
@@ -588,6 +588,34 @@ def generate(config):
     return None
 
 
+def initialize_interface(iface, driver, iface_config):
+    # DPDK - rescan PCI to use a proper driver
+    if driver == 'dpdk' and iface_config['original_driver'] not in not_pci_drv:
+        control_host.pci_rescan(iface_config['dev_id'])
+        # rename to the proper name
+        iface_new_name: str = control_host.get_eth_name(iface_config['dev_id'])
+        control_host.rename_iface(iface_new_name, iface)
+
+    # XDP - rename an interface, disable promisc and XDP
+    if driver == 'xdp':
+        control_host.set_promisc(f'defunct_{iface}', 'off')
+        control_host.rename_iface(f'defunct_{iface}', iface)
+        control_host.xdp_remove(iface)
+
+    # Rename Mellanox NIC to a normal name
+    try:
+        if control_host.get_eth_driver(f'defunct_{iface}') == 'mlx5_core':
+            control_host.rename_iface(f'defunct_{iface}', iface)
+    except FileNotFoundError:
+        pass
+
+    # Replace a driver with original for VMBus interfaces and rename it
+    if driver == 'dpdk' and iface_config['original_driver'] in override_drivers:
+        control_host.override_driver(iface_config['bus_id'], iface_config['dev_id'])
+        iface_new_name: str = control_host.get_eth_name(iface_config['dev_id'])
+        control_host.rename_iface(iface_new_name, iface)
+
+
 def apply(config):
     # Open persistent config
     # It is required for operations with interfaces
@@ -673,7 +701,27 @@ def apply(config):
         # connect to VPP
         # must be performed multiple attempts because API is not available
         # immediately after the service restart
-        vpp_control = VPPControl(attempts=20, interval=500)
+#
+#        vpp_control = VPPControl(attempts=20, interval=500)
+# -- try reinit
+        try:
+            vpp_control = VPPControl(attempts=20, interval=500)
+        except Exception as e:
+            from vyos.config_mgmt import ConfigMgmt
+
+            # if cannot connect then we need initialize interfaces that are still in dataplane
+            Warning(
+                f'An error occurred: {e}\n'
+                f'Some interfaces and options in VPP could be set not properly. '
+                f'Please return to the previous configuration'
+            )
+            for iface, iface_config in config['settings']['interface'].items():
+                initialize_interface(
+                    iface, iface_config['driver'], config['persist_config'][iface]
+                )
+
+            raise ConfigError('use warning msg here')
+
         # preconfigure LCP plugin
         if 'ignore_kernel_routes' in config.get('settings', {}).get('lcp', {}):
             vpp_control.cli_cmd('lcp param route-no-paths off')
